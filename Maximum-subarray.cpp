94addcb3e1662cec14293f16022040c96344c938
AKSHITA GUPTA <57909583+akshitagupta15june@users.noreply.github.com>
2021-04-11 20:06:27 +0530

    Create Maximum-subarray.cpp

---
+++ Maximum-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+    int n = nums.size(), res = nums[0], dp = nums[0];
+        
+        for(int i = 1; i < n; i++) {
+            dp = max(dp+nums[i], nums[i]);
+            
+            res = max(res, dp);
+        }
+        
+        return res;
+    }
+};
